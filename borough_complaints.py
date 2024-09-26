e0fbc6035ed3ac1fa06ddd153343fa2b40df2be4
liweiwang <liweiwang2002@gmail.com>
2024-09-26 02:00:45 -0400

    t1

---
+++ borough_complaints.py
@@ -0,0 +1,35 @@
+import argparse
+import pandas as pd
+from datetime import datetime
+import re
+
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Count complaints per borough for a given date range.')
+    parser.add_argument('-i', '--input', required=True, help='Input CSV file')
+    parser.add_argument('-s', '--start', required=True, help='Start date in MM/DD/YYYY format')
+    parser.add_argument('-e', '--end', required=True, help='End date in MM/DD/YYYY format')
+    parser.add_argument('-o', '--output', help='Optional output CSV file')
+    args = parser.parse_args()
+    
+    df = pd.read_csv(args.input, header=None)
+    df[1] = pd.to_datetime(df[1], format='%m/%d/%Y %I:%M:%S %p')
+    start_date = pd.to_datetime(args.start, format='%m/%d/%Y')
+    end_date = pd.to_datetime(args.end, format='%m/%d/%Y')
+    filtered_df = df.loc[(df[1] >= start_date) & (df[1] <= end_date)]
+
+    filtered_df[23] = filtered_df[23].apply(lambda x: re.sub(r'^\d+\s', '', x))
+    result_df = filtered_df.groupby([5, 23]).size().reset_index(name='count')
+    
+    result_df.columns = ['complaint type', 'borough', 'count']
+
+
+    if args.output:
+        result_df.to_csv(args.output, index=False)
+    else:
+        print(result_df.to_csv(index=False))
+
+if __name__ == '__main__':
+    main()
